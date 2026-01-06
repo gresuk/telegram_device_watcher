fbaf7b102f0d98693aadd7ad7ccd2f9fa192d681
Sviatoslav <92624413+gresuk@users.noreply.github.com>
2026-01-06 14:28:50 +0200

    Update config_flow.py

--- custom_components/telegram_device_watcher/config_flow.py
+++ custom_components/telegram_device_watcher/config_flow.py
@@ -1,8 +1,21 @@
 from homeassistant import config_entries
 from homeassistant.helpers import entity_registry as er
+from homeassistant.helpers import config_validation as cv
+from homeassistant.core import callback
 import voluptuous as vol
 
-from .const import DOMAIN, CONF_ENTITIES
+from .const import DOMAIN, CONF_ENTITIES, CONF_NOTIFY_SERVICE
+
+
+ALLOWED_DOMAINS = {
+    "light",
+    "switch",
+    "binary_sensor",
+    "sensor",
+    "climate",
+    "fan",
+    "cover",
+}
 
 
 class TelegramDeviceWatcherConfigFlow(
@@ -12,16 +25,17 @@ class TelegramDeviceWatcherConfigFlow(
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        # Якщо користувач вже вибрав entity → зберігаємо
         if user_input is not None:
             return self.async_create_entry(
                 title="Telegram Device Watcher",
                 data={
                     CONF_ENTITIES: user_input[CONF_ENTITIES],
                 },
+                options={
+                    CONF_NOTIFY_SERVICE: user_input[CONF_NOTIFY_SERVICE],
+                },
             )
 
-        # Отримуємо всі entity з registry
         entity_reg = er.async_get(self.hass)
 
         entities = {
@@ -30,14 +44,65 @@ class TelegramDeviceWatcherConfigFlow(
                 + (f" ({e.original_name})" if e.original_name else "")
             )
             for e in entity_reg.entities.values()
-            if not e.disabled
+            if e.entity_id.split(".")[0] in ALLOWED_DOMAINS
         }
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema(
                 {
-                    vol.Required(CONF_ENTITIES): vol.MultiSelect(entities)
+                    vol.Required(CONF_ENTITIES): cv.multi_select(entities),
+                    vol.Required(
+                        CONF_NOTIFY_SERVICE,
+                        default="notify.telegram",
+                    ): str,
+                }
+            ),
+        )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return TelegramDeviceWatcherOptionsFlow(config_entry)
+
+
+class TelegramDeviceWatcherOptionsFlow(config_entries.OptionsFlow):
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(
+                title="",
+                data=user_input,
+            )
+
+        entity_reg = er.async_get(self.hass)
+
+        entities = {
+            e.entity_id: (
+                f"{e.entity_id}"
+                + (f" ({e.original_name})" if e.original_name else "")
+            )
+            for e in entity_reg.entities.values()
+            if e.entity_id.split(".")[0] in ALLOWED_DOMAINS
+        }
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(
+                        CONF_ENTITIES,
+                        default=self.config_entry.data.get(CONF_ENTITIES, []),
+                    ): cv.multi_select(entities),
+                    vol.Required(
+                        CONF_NOTIFY_SERVICE,
+                        default=self.config_entry.options.get(
+                            CONF_NOTIFY_SERVICE,
+                            "notify.telegram",
+                        ),
+                    ): str,
                 }
             ),
         )
