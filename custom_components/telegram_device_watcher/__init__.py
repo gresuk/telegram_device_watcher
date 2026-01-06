7802bf721c719d6ee5e9dc6bb070e48d359cb336
Sviatoslav <92624413+gresuk@users.noreply.github.com>
2026-01-06 13:12:50 +0200

    Implement async setup and unload for Telegram watcher

--- custom_components/telegram_device_watcher/__init__.py
+++ custom_components/telegram_device_watcher/__init__.py
@@ -1,1 +1,17 @@
+from .const import DOMAIN
+from .watcher import TelegramDeviceWatcher
+
+async def async_setup_entry(hass, entry):
+    watcher = TelegramDeviceWatcher(hass, entry)
+    await watcher.async_start()
+
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = watcher
+    return True
+
+
+async def async_unload_entry(hass, entry):
+    watcher = hass.data[DOMAIN].pop(entry.entry_id)
+    watcher.async_stop()
+    return True
 
