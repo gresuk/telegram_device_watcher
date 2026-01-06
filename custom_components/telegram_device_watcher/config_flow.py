5edbceb0e547cb21636a8aed89cf279fa0604e5b
Sviatoslav <92624413+gresuk@users.noreply.github.com>
2026-01-06 13:13:04 +0200

    Update config_flow.py

--- custom_components/telegram_device_watcher/config_flow.py
+++ custom_components/telegram_device_watcher/config_flow.py
@@ -1,1 +1,34 @@
+from homeassistant import config_entries
+from homeassistant.helpers import entity_registry as er
+import voluptuous as vol
+
+from .const import DOMAIN, CONF_ENTITIES
+
+
+class TelegramDeviceWatcherConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(
+                title="Telegram Device Watcher",
+                data=user_input,
+            )
+
+        entity_reg = er.async_get(self.hass)
+
+        entities = {
+            e.entity_id: f"{e.entity_id} ({e.original_name or e.entity_id})"
+            for e in entity_reg.entities.values()
+            if not e.disabled
+        }
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_ENTITIES): vol.MultiSelect(entities)
+                }
+            ),
+        )
 
